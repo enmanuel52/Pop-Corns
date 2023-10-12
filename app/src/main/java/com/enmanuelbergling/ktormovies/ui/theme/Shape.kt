ccfd897aa645039521d3d413e7ff6acb8850151f
“enmanuel” <enmanueldelgado52@gmail.com>
2023-10-12 11:44:12 +0100

    updating theme

--- app/src/main/java/com/enmanuelbergling/ktormovies/ui/theme/Shape.kt
+++ app/src/main/java/com/enmanuelbergling/ktormovies/ui/theme/Shape.kt
@@ -5,24 +5,24 @@ import androidx.compose.material3.Shapes
 
 val Shape = Shapes(
     medium = RoundedCornerShape(
-        topStartPercent = 12,
-        bottomEndPercent = 12,
-        topEndPercent = 3,
-        bottomStartPercent = 3,
+        topStartPercent = 16,
+        bottomEndPercent = 16,
+        topEndPercent = 0,
+        bottomStartPercent = 0,
     ),
 
     small = RoundedCornerShape(
-        topStartPercent = 8,
-        bottomEndPercent = 8,
+        topStartPercent = 12,
+        bottomEndPercent = 12,
         topEndPercent = 0,
         bottomStartPercent = 0,
     ),
 
     large = RoundedCornerShape(
-        topStartPercent = 16,
-        bottomEndPercent = 16,
-        topEndPercent = 4,
-        bottomStartPercent = 4,
+        topStartPercent = 20,
+        bottomEndPercent = 20,
+        topEndPercent = 0,
+        bottomStartPercent = 0,
     ),
 
     )
