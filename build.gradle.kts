ef4214544b5d19e4ac41416f2b82a8d23f5bb87b
“enmanuel” <enmanueldelgado52@gmail.com>
2024-01-09 10:49:14 +0100

    to agp 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.github.ben-manes.versions") version "0.50.0" apply false
 }
