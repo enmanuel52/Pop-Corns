449ec7446a0036706fea99f34e8ed4b3da68bb68
“enmanuel” <enmanueldelgado52@gmail.com>
2024-01-15 21:41:36 +0100

    fixing crash related to activity viewmodel

--- app/src/main/java/com/enmanuelbergling/ktormovies/CornTimeVM.kt
+++ app/src/main/java/com/enmanuelbergling/ktormovies/CornTimeVM.kt
@@ -3,18 +3,19 @@ package com.enmanuelbergling.ktormovies
 import com.enmanuelbergling.ktormovies.domain.model.settings.DarkTheme
 import com.enmanuelbergling.ktormovies.domain.usecase.settings.GetDarkThemeUC
 import com.enmanuelbergling.ktormovies.domain.usecase.settings.SetDarkThemeUC
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import moe.tlaster.precompose.viewmodel.ViewModel
 import moe.tlaster.precompose.viewmodel.viewModelScope
 
 class CornTimeVM(
-    getDarkThemeUC: GetDarkThemeUC,
-    private val setDarkThemeUC: SetDarkThemeUC,
+//    getDarkThemeUC: GetDarkThemeUC,
+//    private val setDarkThemeUC: SetDarkThemeUC,
 ) : ViewModel() {
 
-    val darkTheme = getDarkThemeUC()
+    val darkTheme = /*getDarkThemeUC()*/flow<DarkTheme> {  }
 
     fun setDarkTheme(darkTheme: DarkTheme) = viewModelScope.launch {
-        setDarkThemeUC(darkTheme)
+//        setDarkThemeUC(darkTheme)
     }
 }
