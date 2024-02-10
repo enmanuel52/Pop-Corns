4ff445b75621aefc8536cc279f8360aa7dd9c3e9
“enmanuel” <enmanueldelgado52@gmail.com>
2024-02-10 21:14:52 +0100

    isAppending function added

--- app/src/main/java/com/enmanuelbergling/ktormovies/ui/core/Extension.kt
+++ app/src/main/java/com/enmanuelbergling/ktormovies/ui/core/Extension.kt
@@ -28,6 +28,10 @@ fun Modifier.shimmerIf(condition: () -> Boolean) = if (condition()) this then sh
 
 val LazyPagingItems<*>.isRefreshing: Boolean
     get() = loadState.refresh == LoadState.Loading
+
+val LazyPagingItems<*>.isAppending: Boolean
+    get() = loadState.append == LoadState.Loading
+
 val LazyPagingItems<*>.isEmpty: Boolean
     get() =  itemCount == 0
 
