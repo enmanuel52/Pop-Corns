f2e7ac71f526cbcb16b8eb66efefcf8446247243
“enmanuel” <enmanueldelgado52@gmail.com>
2024-02-14 15:33:50 +0100

    adding gradle logic to update to stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
-    } 
+    }
 }
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -11,4 +13,19 @@ plugins {
     id("com.github.ben-manes.versions") version "0.50.0" apply false
     id("nl.littlerobots.version-catalog-update") version "0.8.3" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+
+// https://github.com/ben-manes/gradle-versions-plugin
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
