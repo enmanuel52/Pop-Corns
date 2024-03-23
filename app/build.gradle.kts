5cd71b4c9e8f2846fe5d9bb0c201d2ff1bd53162
“enmanuel” <enmanueldelgado52@gmail.com>
2024-03-23 00:48:49 -0400

    using customized plugin for app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,31 +1,15 @@
-import java.io.FileInputStream
-import java.util.Properties
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+   alias(libs.plugins.corntime.android.application)
+   alias(libs.plugins.corntime.android.compose)
 }
-
-val secretFile = rootProject.file("secret.properties")
-val secretProperties = Properties()
-secretProperties.load(FileInputStream(secretFile))
 
 android {
     namespace = "com.enmanuelbergling.ktormovies"
-    compileSdk = 34
 
     defaultConfig {
-        configurations.all {
-            resolutionStrategy {
-                force("androidx.emoji2:emoji2-views-helper:1.3.0")
-                force("androidx.emoji2:emoji2:1.3.0")
-                force("androidx.activity:activity-compose:1.7.2")
-                exclude("androidx.fragment", "fragment")
-            }
-        }
 
         applicationId = "com.enmanuelbergling.ktormovies"
-        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -34,47 +18,8 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-
-        buildConfigField(
-            type = "String",
-            name = "API_KEY",
-            value = "\"${secretProperties.getProperty("API_KEY")}\""
-        )
-
-        buildConfigField(
-            type = "String",
-            name = "ACCOUNT_ID",
-            value = "\"${secretProperties.getProperty("ACCOUNT_ID")}\""
-        )
     }
 
-    buildFeatures {
-        buildConfig = true
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -108,10 +53,6 @@ dependencies {
     implementation(libs.koin.core)
 
     implementation(libs.androidx.core.splashscreen)
-
-    //paging
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.androidx.paging.paging.compose)
 
     //Navigation
     implementation(libs.moe.tlaster.precompose)
