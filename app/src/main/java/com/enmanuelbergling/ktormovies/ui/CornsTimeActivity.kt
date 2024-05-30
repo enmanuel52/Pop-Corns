76957cf2aca2467d2715486b7c0b825db78b54a1
“enmanuel” <enmanueldelgado52@gmail.com>
2024-05-30 14:40:41 -0400

    fixing NullPointerException with intents

--- app/src/main/java/com/enmanuelbergling/ktormovies/ui/CornsTimeActivity.kt
+++ app/src/main/java/com/enmanuelbergling/ktormovies/ui/CornsTimeActivity.kt
@@ -1,7 +1,5 @@
 package com.enmanuelbergling.ktormovies.ui
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,12 +11,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.enmanuelbergling.core.common.android_util.ShortCutModel
-import com.enmanuelbergling.core.common.android_util.addDynamicShortCut
 import com.enmanuelbergling.core.common.android_util.isOnline
 import com.enmanuelbergling.core.common.android_util.removeAllDynamicShortCuts
 import com.enmanuelbergling.core.model.settings.DarkTheme
-import com.enmanuelbergling.core.ui.model.WatchlistShortcut
 import com.enmanuelbergling.core.ui.theme.CornTimeTheme
 import com.enmanuelbergling.feature.movies.navigation.navigateToMovieSearch
 import com.enmanuelbergling.feature.watchlists.navigation.navigateToListDetailsScreen
@@ -32,18 +27,18 @@ class CornsTimeActivity : ComponentActivity(), KoinComponent {
 
     private val viewModel: CornTimeVM by inject()
 
-    private val searchMovieShortCutClicked = intent.getStringExtra("search_movie_extra") != null
-
-    private val watchlistShortCutId =
-        intent.getIntExtra(getString(R.string.watch_list_id_extra), NO_WATCHLIST)
-
-    private val watchListShortCutName =
-        intent.getStringExtra(getString(R.string.watch_list_name_extra)).orEmpty()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         installSplashScreen()
+
+        val searchMovieShortCutClicked = intent.getStringExtra("search_movie_extra") != null
+
+        val watchlistShortCutId =
+            intent.getIntExtra(getString(R.string.watch_list_id_extra), NO_WATCHLIST)
+
+        val watchListShortCutName =
+            intent.getStringExtra(getString(R.string.watch_list_name_extra)).orEmpty()
 
         setContent {
 
