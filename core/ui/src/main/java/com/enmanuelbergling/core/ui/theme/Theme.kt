d48495fe112a30a44b36dddeea36b17e905bbc15
enmanuel <enmanueldelgado52@gmail.com>
2026-06-15 12:33:10 -0300

    refactor: Update Material 3 color palette and typography

--- core/ui/src/main/java/com/enmanuelbergling/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/enmanuelbergling/core/ui/theme/Theme.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -251,6 +252,17 @@ private val highContrastDarkColorScheme = darkColorScheme(
     surfaceContainerHighest = surfaceContainerHighestDarkHighContrast,
 )
 
+@Immutable
+data class ColorFamily(
+    val color: Color,
+    val onColor: Color,
+    val colorContainer: Color,
+    val onColorContainer: Color
+)
+
+val unspecified_scheme = ColorFamily(
+    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
+)
 @Composable
 fun CornTimeTheme(
     darkTheme: DarkTheme = DarkTheme.System,
--- core/ui/src/main/java/com/enmanuelbergling/core/ui/theme/Type.kt
+++ core/ui/src/main/java/com/enmanuelbergling/core/ui/theme/Type.kt
@@ -15,14 +15,14 @@ val provider = GoogleFont.Provider(
 
 val bodyFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Nunito Sans"),
+        googleFont = GoogleFont("Inter"),
         fontProvider = provider,
     )
 )
 
 val displayFontFamily = FontFamily(
     Font(
-        googleFont = GoogleFont("Signika Negative"),
+        googleFont = GoogleFont("Urbanist"),
         fontProvider = provider,
     )
 )
