686ae71e56cc4a19ddc8ec890757a5c30620ed58
enmanuel <enmanueldelgado52@gmail.com>
2026-06-27 23:59:13 -0300

    refactor: SeasonsScreen and EpisodesScreen list items

--- feature/series/src/main/java/com/enmanuelbergling/feature/series/episodes/EpisodesScreen.kt
+++ feature/series/src/main/java/com/enmanuelbergling/feature/series/episodes/EpisodesScreen.kt
@@ -1,15 +1,14 @@
 package com.enmanuelbergling.feature.series.episodes
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -20,9 +19,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.ArrowForwardIos
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -40,6 +39,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -146,69 +146,69 @@ private fun EpisodeRow(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { expanded = !expanded }
-            .animateContentSize(spring(Spring.DampingRatioLowBouncy, Spring.StiffnessLow))
+            .clip(CardDefaults.shape)
+            .combinedClickable(onLongClick = { expanded = !expanded }, onClick = onOpen)
     ) {
-        Row(
-            modifier = Modifier.padding(MaterialTheme.dimen.small),
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier
+                .padding(all = MaterialTheme.dimen.small),
         ) {
-            AsyncImage(
-                model = BASE_BACKDROP_IMAGE_URL + episode.stillPath.orEmpty(),
-                contentDescription = "episode still",
-                placeholder = painterResource(CoreR.drawable.pop_corn_and_cinema_backdrop),
-                error = painterResource(CoreR.drawable.pop_corn_and_cinema_backdrop),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.width(96.dp)
-            )
-
-            Spacer(Modifier.width(MaterialTheme.dimen.small))
-
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "${episode.episodeNumber}. ${episode.name}",
-                    style = MaterialTheme.typography.titleMedium,
-                    fontWeight = FontWeight.SemiBold,
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AsyncImage(
+                    model = BASE_BACKDROP_IMAGE_URL + episode.stillPath.orEmpty(),
+                    contentDescription = "episode still",
+                    placeholder = painterResource(CoreR.drawable.pop_corn_and_cinema_backdrop),
+                    error = painterResource(CoreR.drawable.pop_corn_and_cinema_backdrop),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(70.dp)
+                        .aspectRatio(1.5f)
+                        .clip(CardDefaults.shape)
                 )
 
-                if (expanded) {
-                    Spacer(Modifier.height(MaterialTheme.dimen.verySmall))
+                Spacer(Modifier.width(MaterialTheme.dimen.small))
 
-                    if (episode.overview.isNotBlank()) {
-                        Text(
-                            text = episode.overview,
-                            style = MaterialTheme.typography.bodyMedium,
-                            maxLines = 3,
-                            overflow = TextOverflow.Ellipsis,
-                        )
-                    }
-
-                    Spacer(Modifier.height(MaterialTheme.dimen.verySmall))
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(MaterialTheme.dimen.verySmall),
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = "${episode.episodeNumber}. ${episode.name}",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.SemiBold,
+                    )
 
                     Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            text = episode.duration,
-                            style = MaterialTheme.typography.bodySmall,
-                            fontWeight = FontWeight.Light,
-                        )
-
-                        Spacer(Modifier.width(MaterialTheme.dimen.small))
-
                         RatingStars(
                             value = episode.voteAverage.div(2).toFloat(),
                             size = 14.dp,
                             spaceBetween = 1.dp
                         )
+
+                        Spacer(Modifier.width(MaterialTheme.dimen.small))
+
+                        Text(
+                            text = episode.duration,
+                            style = MaterialTheme.typography.bodySmall,
+                            fontWeight = FontWeight.Light,
+                        )
                     }
                 }
+
             }
 
-            IconButton(onClick = onOpen) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Rounded.ArrowForwardIos,
-                    contentDescription = stringResource(R.string.episode_details)
+            AnimatedVisibility(expanded && episode.overview.isNotBlank()) {
+                Text(
+                    text = episode.overview,
+                    style = MaterialTheme.typography.bodyMedium,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(top = MaterialTheme.dimen.verySmall)
                 )
             }
         }
+
     }
 }
--- feature/series/src/main/java/com/enmanuelbergling/feature/series/seasons/SeasonsScreen.kt
+++ feature/series/src/main/java/com/enmanuelbergling/feature/series/seasons/SeasonsScreen.kt
@@ -1,16 +1,14 @@
 package com.enmanuelbergling.feature.series.seasons
 
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
@@ -20,9 +18,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.rounded.ArrowForwardIos
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -42,6 +40,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
@@ -190,15 +189,15 @@ private fun SeasonsScreen(
 @Composable
 private fun SeasonRow(
     season: Season,
+    modifier: Modifier = Modifier,
     onOpen: () -> Unit,
 ) {
     var expanded by remember { mutableStateOf(false) }
 
     Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { expanded = !expanded }
-            .animateContentSize(spring(Spring.DampingRatioLowBouncy, Spring.StiffnessLow))
+        modifier = modifier
+            .clip(CardDefaults.shape)
+            .combinedClickable(onLongClick = { expanded = !expanded }, onClick = onOpen)
     ) {
         Row(
             modifier = Modifier.padding(MaterialTheme.dimen.small),
@@ -210,7 +209,14 @@ private fun SeasonRow(
                 placeholder = painterResource(CoreR.drawable.pop_corn_and_cinema_poster),
                 error = painterResource(CoreR.drawable.pop_corn_and_cinema_poster),
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.width(64.dp)
+                modifier = Modifier
+                    .padding(
+                        horizontal = MaterialTheme.dimen.small,
+                        vertical = MaterialTheme.dimen.verySmall
+                    )
+                    .height(90.dp)
+                    .aspectRatio(0.65f)
+                    .clip(CardDefaults.elevatedShape)
             )
 
             Spacer()
@@ -226,22 +232,15 @@ private fun SeasonRow(
                     style = MaterialTheme.typography.bodySmall,
                 )
 
-                if (expanded && season.overview.isNotBlank()) {
+                AnimatedVisibility(expanded && season.overview.isNotBlank()) {
                     Spacer()
                     Text(
                         text = season.overview,
                         style = MaterialTheme.typography.bodyMedium,
-                        maxLines = 3,
+                        maxLines = 4,
                         overflow = TextOverflow.Ellipsis,
                     )
                 }
-            }
-
-            IconButton(onClick = onOpen) {
-                Icon(
-                    imageVector = Icons.AutoMirrored.Rounded.ArrowForwardIos,
-                    contentDescription = stringResource(R.string.episodes)
-                )
             }
         }
     }
